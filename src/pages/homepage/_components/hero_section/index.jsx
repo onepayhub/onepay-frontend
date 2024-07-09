ea2ecff8eb4fcd861a9456e0afdecc588ba8d8b9
encodedevv <timothyonyea@gmail.com>
2024-07-09 01:10:10 +0100

    style fix to stickers

--- src/pages/homepage/_components/hero_section/index.jsx
+++ src/pages/homepage/_components/hero_section/index.jsx
@@ -13,21 +13,21 @@ import {
 
 const HeroSection = () => {
   return (
-    <div className="bg-[#eee] relative px-10 rounded-[20px] py-10 mt-20 container mx-auto container-md">
+    <div className="bg-[#eee] relative px-10 rounded-[20px] py-10 lg:mt-20 mt-0 rounded-tr-none rounded-tl-none container mx-auto container-md">
       <img
         src={image2}
         alt="image"
-        className="absolute w-[70px] lg:w-[100px] bottom-[30%]"
+        className="absolute w-[70px] lg:w-[100px] left-[0%] lg:left-[2%] bottom-[30%]"
       />
       <img
         src={image6}
         alt="image"
-        className="absolute w-[70px] lg:w-[100px]  right-0 top-[0%]"
+        className="absolute w-[70px] lg:w-[100px] right-0 top-[0%]"
       />
       <img
         src={image7}
         alt="image"
-        className="absolute w-[70px] lg:w-[100px] left-[11%] bottom-[7%]"
+        className="absolute w-[70px] lg:w-[100px] left-[20%] lg:left-[11%] bottom-[7%]"
       />
       <img
         src={image9}
@@ -37,12 +37,12 @@ const HeroSection = () => {
       <img
         src={image5}
         alt="image"
-        className="absolute w-[70px] lg:w-[100px] bottom-[3%] right-[1%]"
+        className="absolute w-[70px] lg:w-[100px] bottom-[3%] right-[3%] md:right-[1%]"
       />
       <img
         src={image4}
         alt="image"
-        className="absolute w-[70px] lg:w-[100px] bottom-[30%] right-[5%]"
+        className="absolute w-[70px] lg:w-[100px] bottom-[20%] lg:bottom-[30%] right-[5%]"
       />
       <div className="flex gap-y-2 justify-center h-[50vh]  mx-auto  items-center flex-col">
         <div className="flex flex-col tracking-widest">

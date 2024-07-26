a0b3724e05472e15dca8e4ba46e81f0f0c3ecb3c
encodedevv <timothyonyea@gmail.com>
2024-07-26 09:17:50 +0100

    update to modals

--- src/pages/dashboard/_components/home/_components/approve-modal/index.jsx
+++ src/pages/dashboard/_components/home/_components/approve-modal/index.jsx
@@ -4,22 +4,61 @@ import { setApprovePayment } from "../../../../../../slice/dashboard";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { IoCheckmark, IoWalletSharp } from "react-icons/io5";
 import { Button } from "../../../../../../components";
+import {
+  AccessLogo,
+  FbLogo,
+  Fidelity,
+  GtbankLogo,
+} from "../../../../../../constants/images";
 
 const ApprovePayment = () => {
   const [filteredNotifications, setFilteredNotifications] = useState([]);
   const dispatch = useDispatch();
-  const allNotifications = useSelector((state) => state?.dashboard.states.notificationData);
-  const notificationId = useSelector((state) => state?.dashboard.states.selectedNotificationId);
+  const allNotifications = useSelector(
+    (state) => state?.dashboard.states.notificationData
+  );
+  const notificationId = useSelector(
+    (state) => state?.dashboard.states.selectedNotificationId
+  );
+  const [agreeTerms, setAgreeTerms] = useState(false);
+  const [index, setIndex] = useState(1);
+  const [showBanks, setShowBanks] = useState(false);
+  const [bankName, setBankName] = useState("Select Bank");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     if (allNotifications && allNotifications.length > 0) {
-      const filtered = allNotifications.map(notification => notification.notificationData).filter(notification => notification.id === notificationId);
+      const filtered = allNotifications
+        .map((notification) => notification.notificationData)
+        .filter((notification) => notification.id === notificationId);
       setFilteredNotifications(filtered);
     }
   }, [allNotifications, notificationId]);
 
+  useEffect(() => {
+    if (bankName !== "Select Bank") {
+      setShowBanks(false);
+    }
+  }, [bankName]);
+  const handleBankClick = () => {
+    setShowBanks(!showBanks);
+  };
   const handleCloseModal = () => {
     dispatch(setApprovePayment(false));
+  };
+  const handleNext = () => {
+    if (agreeTerms) {
+      setIndex((prevIndex) => prevIndex + 1);
+      setError("");
+    } else {
+      setError("Please check the box, if you have read and understand fully");
+    }
+  };
+  const handlePrevious = () => {
+    setIndex((prevIndex) => prevIndex - 1); // Increment index on "Next" button click
+  };
+  const handleSetBankName = (value) => {
+    setBankName(value);
   };
 
   return (
@@ -33,12 +72,17 @@ const ApprovePayment = () => {
           onClick={handleCloseModal}
         >
           x
-        </span> 
+        </span>
+
         <div className="flex flex-col gap-y-6">
-            <div className="lg:w-full lg:mx-auto bg-primary lg:bg- lg:rounded-none left-0 right-0 rounded-[20px] lg:static w-full fixed bottom-[.9rem] py-10 flex justify-start items-start">
-              <div className="flex gap-y-2 flex-col w-full">
-                {filteredNotifications.map((notification) => (
-                  <div className="flex gap-y-3 flex-col relative" key={notification.id}>
+          <div className="lg:w-full lg:mx-auto bg-primary lg:bg- lg:rounded-none left-0 right-0 rounded-[20px] lg:static w-full fixed bottom-[.9rem] py-10 flex justify-start items-start">
+            <div className="flex gap-y-2 flex-col w-full">
+              {index === 1 &&
+                filteredNotifications.map((notification) => (
+                  <div
+                    className="flex gap-y-3 flex-col relative"
+                    key={notification.id}
+                  >
                     <span
                       className="text-xl cursor-pointer block lg:hidden right-6 lg:right-10 top-[-5%] lg:top-6 z-20 absolute"
                       onClick={handleCloseModal}
@@ -102,7 +146,9 @@ const ApprovePayment = () => {
                       <span className="text-sm text-lightgray">Reason</span>
                       <div className="flex gap-x-2 items-center">
                         <span className="text-xs lg:text-sm text-[#181818]">
-                        {notification.reason.length > 10 ? `${notification.reason.slice(0, 30)}...` : notification.reason}
+                          {notification.reason.length > 10
+                            ? `${notification.reason.slice(0, 30)}...`
+                            : notification.reason}
                         </span>
                       </div>
                     </div>
@@ -112,8 +158,9 @@ const ApprovePayment = () => {
                           type="checkbox"
                           name="agreeTerms"
                           id="agreeTerms"
-                          // checked={details.agreeTerms}
-                          // onChange={handleForm}
+                          className="cursor-pointer"
+                          checked={agreeTerms}
+                          onChange={(e) => setAgreeTerms(e.target.value)}
                         />
                         <span className="text-lightgray text-xs">
                           I have confirmed that the above information is
@@ -124,6 +171,9 @@ const ApprovePayment = () => {
                           of the payment request or debt owed.
                         </span>
                       </div>
+                      {error && (
+                        <span className="text-xs text-[#e62e2e]">{error}</span>
+                      )}
                     </div>
                     <div className="bg-[#dadada] mx-4  text-lightgray justify-between rounded-[10px] py-4 lg:py-6 px-4 flex items-center">
                       <div className="flex items-center lg:text-base text-sm gap-x-2">
@@ -136,14 +186,119 @@ const ApprovePayment = () => {
                       <IoCheckmark color="323b6d" />
                     </div>
                     <div className="w-full pt-8 flex justify-center">
-                      <Button className="w-5/6 mx-auto">Approve payment</Button>
+                      <Button className="w-5/6 mx-auto" onClick={handleNext}>
+                        Next
+                      </Button>
                     </div>
                   </div>
                 ))}
-              </div>
-            </div>
 
-  
+              {index === 2 && (
+                <div className="w-full px-6 h-full">
+                  {filteredNotifications.map((value, i) => (
+                    <div
+                      key={i}
+                      className="flex w-full flex-col gap-y-10 relative h-[30vh] overflow-auto"
+                    >
+                      <span
+                      className="text-xl cursor-pointer block lg:hidden right-2 lg:right-10 top-[5%] lg:top-6 z-20 absolute"
+                      onClick={handleCloseModal}
+                    >
+                      x
+                    </span>
+                      <input
+                        type="number"
+                        className="py-2 px-4 border border-[#5f5f5f] rounded-[5px] w-5/6"
+                        value={value.recipientId}
+                        name="id"
+                        contentEditable={false}
+                      />
+                      <div
+                        className="w-5/6 flex items-center py-2 px-4 border rounded-[5px] cursor-pointer border-lightgray"
+                        onClick={handleBankClick}
+                      >
+                        <span className="text-lightgray font-medium w-full">
+                          {bankName}
+                        </span>
+                        <MdOutlineKeyboardArrowLeft
+                          size={20}
+                          className={`rotate-180 ${
+                            showBanks && "-rotate-90 transition-all ease-in"
+                          }`}
+                        />
+                      </div>
+                      {showBanks && (
+                        <div className="absolute h-[50vh] overflow-auto w-full top-[8rem] animate-slide_up lg:w-5/6">
+                          <div className="bg-[#f5f5f5] text-sm lg:text-base flex flex-col px-2 py-3 rounded-[5px]">
+                            <div
+                              className="flex items-center gap-x-4 py-2 lg:py-3 cursor-pointer"
+                              onClick={() => handleSetBankName("Access Bank")}
+                            >
+                              <img
+                                src={AccessLogo}
+                                alt="bank"
+                                className="w-[30px]"
+                              />
+                              <span>Access Bank</span>
+                            </div>
+                            <hr color="#5f5f5f" />
+
+                            <div
+                              className="flex items-center gap-x-4 py-2 lg:py-3  cursor-pointer"
+                              onClick={() =>
+                                handleSetBankName("Guaranty Trust Bank")
+                              }
+                            >
+                              <img
+                                src={GtbankLogo}
+                                alt="bank"
+                                className="w-[30px]"
+                              />
+                              <span>Guaranty Trust Bank</span>
+                            </div>
+                            <hr color="#5f5f5f" />
+
+                            <div
+                              className="flex items-center gap-x-4 py-2 lg:py-3  cursor-pointer"
+                              onClick={() => handleSetBankName("Fidelity Bank")}
+                            >
+                              <img
+                                src={Fidelity}
+                                alt="bank"
+                                className="w-[30px]"
+                              />
+                              <span>Fidelity Bank</span>
+                            </div>
+                            <hr color="#5f5f5f" />
+
+                            <div
+                              className="flex items-center text-[#181818] gap-x-4 py-2 lg:py-3  cursor-pointer"
+                              onClick={() => handleSetBankName("First Bank")}
+                            >
+                              <img
+                                src={FbLogo}
+                                alt="bank"
+                                className="w-[30px]"
+                              />
+                              <span>First Bank</span>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                  <div className="flex pt-16 gap-x-2 items-center w-full lg:justify-center">
+                    <Button onClick={handlePrevious} className="w-[150px]">
+                      Prev
+                    </Button>
+                    <Button onClick={handlePrevious} className="w-[150px]">
+                      Approve
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
